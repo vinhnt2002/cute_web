72a42ac91efa16c3d7b45502889c0a62d6db55b8
vinh <124482049+vinhnt2002@users.noreply.github.com>
2025-03-08 10:35:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -205,7 +205,7 @@ function App() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        Chúc mừng ngày 8/3, cún iu cụa anhhh!
+        Chúc mừng ngày 8/3!
       </Title>
       
       <PuppyContainer
